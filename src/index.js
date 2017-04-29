8954b202e129ddbe2b6abf6f28039942a62437d4
jenessawhite <jenessaw31@gmail.com>
2017-04-29 10:26:20 -0500

    may 1 reboot

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
-// var line = new ProgressBar.Line('#container');
-//
-// var bar = new ProgressBar.SemiCircle(container, {
-//   strokeWidth: 6,
-//   color: '#FFEA82',
-//   trailColor: '#eee',
-//   trailWidth: 1,
-//   easing: 'easeInOut',
-//   duration: 1400,
-//   svgStyle: null,
-//   text: {
-//     value: '',
-//     alignToBottom: false
-//   },
-//   from: {color: '#FFEA82'},
-//   to: {color: '#ED6A5A'},
-//   // Set default step function for all animate calls
-//   step: (state, bar) => {
-//     bar.path.setAttribute('stroke', state.color);
-//     var value = Math.round(bar.value() * 100);
-//     if (value === 0) {
-//       bar.setText('');
-//     } else {
-//       bar.setText(value);
-//     }
-//
-//     bar.text.style.color = state.color;
-//   }
-// });
-// bar.text.style.fontFamily = 'sans-serif';
-// bar.text.style.fontSize = '2rem';
-//
-// bar.animate(1.0);  // Number from 0.0 to 1.0
+// Set the date
+var countDownDate = new Date("May 1, 2017 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "It's go time";
+    }
+}, 1000);
