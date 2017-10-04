568ca45d3c016d1ab0300f0869894a97147eda19
Jenessa White <jenessaw31@gmail.com>
2017-10-03 22:59:40 -0500

    add gatsby react-md starter

---
+++ JenessaSite/data/SiteConfig.js
@@ -0,0 +1,39 @@
+module.exports = {
+  blogPostDir: "blog-posts", // The name of directory that contains your posts.
+  siteTitle: "Jenessa White", // Site title.
+  siteTitleAlt: "Front-End Engineer by day...", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "http://jenessawhite.com", // Domain of your website without pathPrefix.
+  pathPrefix: "/jenessawhite", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "", // FB Application ID for using app insights
+  siteGATrackingID: "UA-88259176-1", // Tracking code ID for google analytics.
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "Jenessa White", // Username to display in the author segment.
+  userTwitter: "jnessview", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Twin Cities", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription:
+    "", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/jenessawhite",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/jnessview",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Email",
+      url: "mailto:jenessaw31@gmail.com",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "© 2017. Jenessa White" // Copyright string for the footer of the website and RSS feed.
+};
