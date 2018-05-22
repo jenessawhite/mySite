95cdec048cb537a861e38182cac49e6b0e40a040
Jenessa White <jenessaw31@gmail.com>
2018-05-21 23:20:49 -0500

    contact form

--- new-site/src/containers/Contact.js
+++ new-site/src/containers/Contact.js
@@ -3,8 +3,41 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="content">
         <h1>Contact</h1>
+        <p>
+          Want to meet up? <strong> or </strong> Work together?
+        </p>
+
+        <p>well here's your chance...
+        <br/>but don't be a douche. I'll just report you as spam and keep it pushing...</p>
+
+        <form action="https://formspree.io/jenessaw31@gmail.com"
+          method="POST">
+
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input className="input" type="text" name="name" placeholder="your name" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input className="input" type="email" name="_replyto" placeholder="your email"/>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Message</label>
+            <div className="control">
+              <textarea className="textarea" type="textarea" name="message" placeholder="your message" />
+            </div>
+          </div>
+
+          <input type="submit" value="Send" className="button is-primary"/>
+        </form>
       </div>
     );
   }
--- new-site/src/containers/Portfolio.js
+++ new-site/src/containers/Portfolio.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Portfolio extends Component {
   render() {
     return (
-      <div>
+      <div className="content">
         <h1>Portfolio</h1>
       </div>
     );
